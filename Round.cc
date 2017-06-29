8d555df84b7717ef3b83eec1619e0526a1767cea
mrdofu <intense_smurf@live.com>
2017-06-29 11:59:18 -0400

    added code to print cards played in the round on human player

--- Round.cc
+++ Round.cc
@@ -17,3 +17,36 @@ void Round::startRound() {
 	// the round is over
 
 }
+
+/**
+ * Prints ordered sequence of all the ranks in that suit (e.g., 6 7 8 9 T J Q) that have already been
+ * played.
+ */
+void Round::printSuitPlayed(int suit) {
+	std::vector<Card> toPrint;
+	switch (suit) {
+	case 0:
+		toPrint = clubsPlayed;
+		break;
+	case 1:
+		toPrint = diamondsPlayed;
+		break;
+	case 2:
+		toPrint = heartsPlayed;
+		break;
+	case 3:
+		toPrint = spadesPlayed;
+		break;
+	}
+	for (int i = 0; i < toPrint.size(); i++) {
+		std::cout << " " << toPrint[i].rank().rank();
+	}
+}
+
+void Round::printStatus() {
+	std::cout << "Cards on the table:" << std::endl
+		<< "Clubs:"; printSuitPlayed(0); std::cout << std::endl
+		<< "Diamonds:"; printSuitPlayed(1); std::cout << std::endl
+		<< "Hearts:"; printSuitPlayed(2); std::cout << std::endl
+		<< "Spades:"; printSuitPlayed(3); std::cout << std::endl;
+}
--- Round.h
+++ Round.h
@@ -4,13 +4,21 @@
 #include <vector>
 #include <memory>
 #include "Player.h"
+#include "Card.h"
 
 class Round {
 private:
 	std::vector<std::shared_ptr<Player>> players;
+	std::vector<Card> clubsPlayed;		// cards played are stored in order (A 2 3 4)
+	std::vector<Card> diamondsPlayed;
+	std::vector<Card> heartsPlayed;
+	std::vector<Card> spadesPlayed;
+	void queryPlayer(std::shared_ptr<Player> p);
+	void printSuitPlayed(int suit);
 public:
 	Round(std::vector<std::shared_ptr<Player>> players);
 	void startRound();
+	void printStatus();
 };
 
 #endif
